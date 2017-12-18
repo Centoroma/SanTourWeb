edcadb20e6d0ca9f5969846fc3a9ce49ef090645
kevdlc <32674096+kevdlc@users.noreply.github.com>
2017-12-18 10:14:19 +0100

    Details tracks + edit track

--- app/view/tracks/view-details.php
+++ app/view/tracks/view-details.php
@@ -4,82 +4,156 @@
     </div>
     <br/>
     <br/>
+
     <div class="row">
-        <div class="row">
-            <div class="col m12 resa-bold-span">
-                <div class="row">
-                    <div class="col s12 m6 l3">
-                        <span>Name :</span>
-                        <?php echo $track->getName(); ?>
-                    </div>
-
-                </div>
-                <div class="row">
-                    <div class="col s12 m6 l3">
-                        <span>Duration : </span>
-                        <?php echo $track->getTimer(); ?>
-                    </div>
-
-                </div>
-                <div class="row">
-                    <div class="col s12 m6 l3">
-                        <span>Length :</span>
-                        <?php echo $track->getLength(); ?>
-                    </div>
+        <div class="col m12 resa-bold-span">
+            <div class="row">
+                <div class="col s12 m6 l3">
+                    <span>Name :</span>
+                    <?php echo $track->getName(); ?>
                 </div>
 
-                <div class="row">
-                    <div class="col s12 m6 l3">
-                        <span>Coordonnées : </span>
+            </div>
+            <div class="row">
+                <div class="col s12 m6 l3">
+                    <span>Duration : </span>
+                    <?php echo $track->getTimer(); ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m6 l3">
+                    <span>Length :</span>
+                    <?php echo $track->getLength(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                        <?php
-                        $html = "";
-                        '<ul>';
-                        foreach ($track->getCoordinate() as $coord) {
+    <div class="row">
+        <div class="col m2">
 
-                            $html .= '
+            <h5>Coordonnées : </h5>
+
+            <?php
+            $html = "";
+            '<ul>';
+            foreach ($track->getCoordinate() as $coord) {
+
+                $html .= '
                 
                                    <li style="list-style-type: none"><i class="material-icons">location_on</i>' . $coord->getLongitude() . ' / ' . $coord->getLatitude() . '</li>
                
                             ';
-                        }
+            }
 
-                        echo $html;
-                        '</ul>';
-                        ?>
+            echo $html;
+            '</ul>';
+            ?>
 
-                    </div>
-                </div>
+        </div>
 
-                <br/>
-                <hr>
-                <br/>
-                <br/>
+        <div class="col m10">
+            <!DOCTYPE html>
+
+
+            <html>
+            <head>
+                <style>
+                    #map {
+                        height: 400px;
+                        width: 100%;
+                    }
+                </style>
+            </head>
+            <body>
+
+            <div id="map"></div>
+            <script>
+                function initMap() {
+
+
+                    var coordinates = [
+                        <?php foreach ($track->getCoordinate() as $coord) {
+                        echo '{lat:' . $coord->getLatitude() . ', lng:' . $coord->getLongitude() . '},';
+                    } ?>
+
+                    ];
+
+                    var mapFocus = coordinates[0];
+
+                    var map = new google.maps.Map(document.getElementById('map'), {
+
+
+                        zoom: 16,
+                        center: mapFocus
+                    });
+
+
+                    console.log(coordinates);
+
+
+                    var markerDebut = new google.maps.Marker({
+                        position: coordinates[0],
+
+                        map: map
+                    });
+
+                    var markerFin = new google.maps.Marker({
+                        position: coordinates[coordinates.length - 1],
+                        
+                        map: map
+                    });
+
+
+                    var flightPath = new google.maps.Polyline({
+                        path: coordinates,
+                        geodesic: true,
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 2
+                    });
+
+                    flightPath.setMap(map);
+
+                }
+            </script>
+            <script async defer
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDy8PTk9AjlPiRUnx8imS4R0hbWrYdPtbQ&callback=initMap">
+            </script>
+            </body>
+            </html>
+        </div>
+    </div>
+
+    <br/>
+    <hr>
+
+
+    <div class="row">
+        <div class="col m6">
+            <div class="resa-title">
+                <h4>POI List</h4>
             </div>
-            <div class="col m6">
-                <div class="resa-title">
-                    <h4>POI List</h4>
-                </div>
 
 
-                <table class="bordered">
-                    <thead>
-                    <tr>
-                        <th>Coordinates</th>
-                        <th>Description</th>
-                        <th>Name</th>
-                        <th>Picture</th>
-                    </tr>
-                    </thead>
-                    <tbody>
+            <table class="bordered">
+                <thead>
+                <tr>
+                    <th>Coordinates</th>
+                    <th>Description</th>
+                    <th>Name</th>
+                    <th>Picture</th>
+                </tr>
+                </thead>
+                <tbody>
 
-                    <?php
-                    $html = "";
+                <?php
+                $html = "";
 
-                    foreach ($track->getPois() as $poi) {
+                foreach ($track->getPois() as $poi) {
 
 
-                        $html .= '
+                    $html .= '
                 <tr>
                     <td>' . $poi->getCoordinate()->getLongitude() . ' / ' . $poi->getCoordinate()->getLatitude() . '</td>
                     <td>' . $poi->getDescription() . '</td>
@@ -89,39 +163,39 @@
                 </tr>
 
      ';
-                    }
+                }
 
-                    echo $html;
-                    ?>
+                echo $html;
+                ?>
 
-                    </tbody>
-                </table>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="col m6 vl">
+
+            <div class="resa-title">
+                <h4>POD List</h4>
             </div>
 
-            <div class="col m6 vl">
+            <table class="bordered">
+                <thead>
+                <tr>
+                    <th>Coordinate</th>
+                    <th>Description</th>
+                    <th>Name</th>
+                    <th>Picture</th>
+                </tr>
+                </thead>
+                <tbody>
 
-                <div class="resa-title">
-                    <h4>POD List</h4>
-                </div>
+                <?php
+                $html = "";
 
-                <table class="bordered">
-                    <thead>
-                    <tr>
-                        <th>Coordinate</th>
-                        <th>Description</th>
-                        <th>Name</th>
-                        <th>Picture</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    <?php
-                    $html = "";
-
-                    foreach ($track->getPods() as $pod) {
+                foreach ($track->getPods() as $pod) {
 
 
-                        $html .= '
+                    $html .= '
                 <tr>
                     <td>' . $pod->getCoordinate()->getLongitude() . ' / ' . $pod->getCoordinate()->getLatitude() . '</td>
                     <td>' . $pod->getDescription() . '</td>
@@ -131,18 +205,18 @@
                 </tr>
 
                 ';
-                    }
+                }
 
-                    echo $html;
-                    ?>
-                    </tbody>
-                </table>
-
-            </div>
+                echo $html;
+                ?>
+                </tbody>
+            </table>
 
         </div>
 
     </div>
+
+
 </div>
 
 
